2dfedd56f084def16b7e6b819a7ee047dd31cc64
vuylov <vuylov@gmail.com>
2013-12-22 20:12:54 +0400

    Some fixes

--- protected/components/Mark.php
+++ protected/components/Mark.php
@@ -3,9 +3,17 @@ class Mark {
     
     public static function getCourseWithTestMarks($curPeriod, $userId){
         
-        $result = array();
+        $courses = array();
         $now_courses = CoursePeriod::model()->with('course')->findAll('cur_period_id = :c', array(':c'=>$curPeriod));
         
+        foreach ($now_courses as $cP) {
+            array_push($courses, $cP->course->id);
+        }
+        
+        foreach ($courses as $course)
+        {
+            //CourseElement::model()->
+        }
     }
     
 }
